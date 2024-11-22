3c0e143017b8bd550ab3530767bd8ac37150817f
ST10045251 <ST10045251@vcconnect.edu.za>
2024-11-22 22:15:14 +0200

    Made changes to file

--- Models/Claim.cs
+++ Models/Claim.cs
@@ -3,12 +3,12 @@
     public class Claim
     {
         public int ClaimId { get; set; }
-        public string ClaimantName { get; set; }
+        public string ClientName { get; set; }
         public decimal Amount { get; set; }
         public string Status { get; set; }
         public int HoursWorked { get; set; }
         public decimal HourlyRate { get; set; }
-        public DateTime SubissionDate { get; set; }
+        public DateTime SubmissionDate { get; set; }
         public string Notes { get; set; }
     }
 }
